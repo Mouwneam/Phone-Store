a15285a223abc1d23e4b959e9503cd0abd4465c1
Mouwneam <124057919+Mouwneam@users.noreply.github.com>
2025-02-11 21:37:35 +0700

    Fix cors issue

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://phone-store-rosy.vercel.app/",
   optionsSuccessStatus: 200,
 };
 
