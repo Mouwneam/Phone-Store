0e78ab7fdb703e6add9a288ba8ba4c1a4b644037
Mouwneam <124057919+Mouwneam@users.noreply.github.com>
2025-02-11 21:20:09 +0700

    edit backend key again

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 
-const backendKey = process.env.REACT_APP_API_URL;
+const backendKey = process.env.VITE_API_URL;
 
 export const useProductStore = create((set) => ({
   products: [],
