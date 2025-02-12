034215dfef40cf33265e99b9d9210633fcf7dc5c
Mouwneam <124057919+Mouwneam@users.noreply.github.com>
2025-02-12 08:54:51 +0700

    update cors origin(1)

--- backend/server.js
+++ backend/server.js
@@ -8,18 +8,41 @@ dotenv.config();
 
 const app = express();
 
-const corsOptions = {
-  origin: "https://phone-store-rosy.vercel.app",
+const allowlist = [
+  "http://localhost:5173",
+  "https://phone-store-rosy.vercel.app",
+];
+
+/*const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, origin); // Send back the actual origin
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true, // Allow cookies if needed
   optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
+*/
+
+const corsOptionsDelegate = function (req, callback) {
+  let corsOptions;
+  if (allowlist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // allow us to accept JSON data in the req.body
 
-app.use("/api/products", productRoutes);
+app.use("/api/products", cors(corsOptionsDelegate), productRoutes);
 
 app.listen(PORT, () => {
   connectDB();
